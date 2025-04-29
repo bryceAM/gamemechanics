3852a24131ff9b9bfaf6aca15f18dff4180d9307
bryceAM <bryce.malmberg@yahoo.com>
2025-04-28 21:57:48 -0500

    add character creation feature lists

---
+++ creation/lists.js
@@ -0,0 +1,295 @@
+const COMPLEXIONS = {
+    category: 'complexion',
+    options: [
+        {
+            description: 'fair'
+        },
+        {
+            description: 'lily-white'
+        },
+        {
+            description: 'rosy'
+        },
+        {
+            description: 'pallid'
+        },
+        {
+            description: 'ruddy'
+        },
+        {
+            description: 'tanned'
+        },
+        {
+            description: 'white'
+        },
+        {
+            description: 'ivory'
+        },
+        {
+            description: 'alabaster'
+        },
+        {
+            description: 'pale'
+        },
+        {
+            description: 'ashen'
+        },
+        {
+            description: 'snow-white'
+        },
+        {
+            description: 'porcelain'
+        },
+        {
+            description: 'flushed'
+        },
+        {
+            description: 'blushing'
+        },
+        {
+            description: 'swarthy'
+        },
+        {
+            description: 'tawny'
+        },
+        {
+            description: 'bronzed'
+        },
+        {
+            description: 'leathery'
+        },
+        {
+            description: 'dun'
+        },
+        {
+            description: 'ebony'
+        },
+        {
+            description: 'freckled'
+        },
+        {
+            description: 'sable'
+        }
+    ]
+}
+
+const EYECOLORS = {
+    category: 'eye color',
+    options: [
+        {
+            description: 'blue'
+        },
+        {
+            description: 'brown'
+        },
+        {
+            description: 'ale-brown'
+        },
+        {
+            description: 'golden'
+        },
+        {
+            description: 'silver'
+        },
+        {
+            description: 'multihued'
+        },
+        {
+            description: 'green'
+        },
+        {
+            description: 'forest-green'
+        },
+        {
+            description: 'crystal-blue'
+        }
+    ]
+}
+
+const HAIRLENGTHS = {
+    category: 'hair length',
+    options: [
+        {
+            description: 'bald'
+        },
+        {
+            description: 'balding'
+        },
+        {
+            description: 'short'
+        },
+        {
+            description: 'shoulder-length'
+        },
+        {
+            description: 'medium-length'
+        },
+        {
+            description: 'long'
+        },
+        {
+            description: 'ankle-length'
+        }
+    ]
+}
+
+const HAIRSTYLES = {
+    category: 'hair style',
+    options: [
+        {
+            description: 'tonsured'
+        },
+        {
+            description: 'wavy'
+        },
+        {
+            description: 'braided'
+        },
+        {
+            description: 'plaited'
+        },
+        {
+            description: 'tied-back'
+        },
+        {
+            description: 'slicked-back'
+        },
+        {
+            description: 'flowing'
+        },
+        {
+            description: 'tousled'
+        },
+        {
+            description: 'unkempt'
+        },
+        {
+            description: 'bobbed'
+        },
+
+
+        {
+            description: 'silky'
+        },
+        {
+            description: 'smooth'
+        },
+        {
+            description: 'straight'
+        },
+        {
+            description: 'tumbling'
+        },
+        {
+            description: 'cascading'
+        },
+        {
+            description: 'glossy'
+        },
+        {
+            description: 'loose'
+        },
+        {
+            description: 'lustrous'
+        }
+    ]
+}
+
+const HAIRCOLORS = {
+    category: 'hair color',
+    options: [
+        {
+            description: 'blonde'
+        },
+        {
+            description: 'russet'
+        },
+        {
+            description: 'honey-blonde'
+        },
+        {
+            description: 'strawberry-blonde'
+        },
+        {
+            description: 'golden'
+        },
+        {
+            description: 'platinum'
+        },
+        {
+            description: 'gray'
+        },
+        {
+            description: 'silver'
+        },
+        {
+            description: 'pale gray'
+        },
+        {
+            description: 'stormy'
+        },
+        {
+            description: 'steely-gray'
+        },
+        {
+            description: 'brown'
+        },
+        {
+            description: 'chestnut'
+        },
+        {
+            description: 'dark-brown'
+        },
+        {
+            description: 'brunette'
+        },
+        {
+            description: 'auburn'
+        },
+        {
+            description: 'black'
+        },
+        {
+            description: 'midnight-black'
+        },
+        {
+            description: 'jet-black'
+        },
+
+        {
+            description: 'red'
+        },
+        {
+            description: 'claret'
+        },
+        {
+            description: 'fiery-red'
+        },
+        {
+            description: 'titian'
+        },
+        {
+            description: 'scarlet'
+        },
+        {
+            description: 'crimson'
+        },
+        {
+            description: 'amber'
+        },
+        {
+            description: 'white'
+        },
+        {
+            description: 'ginger'
+        }
+    ]
+}
+
+export {
+    COMPLEXIONS,
+    EYECOLORS,
+    HAIRSTYLES,
+    HAIRLENGTHS,
+    HAIRCOLORS
+}
