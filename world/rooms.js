950c9f4734d2397f3ea8e745c36f1005dbf7fcd4
bryceAM <bryce.malmberg@yahoo.com>
2025-04-30 22:42:09 -0500

    refactor rooms to use 1 room for character creation

--- world/rooms.js
+++ world/rooms.js
@@ -1,32 +1,10 @@
-import { commands } from '../utils/commands.js'
-import { listOptions } from '../creation/create.js'
-import { COMPLEXIONS, EYECOLORS, HAIRLENGTHS, HAIRSTYLES, HAIRCOLORS } from '../creation/lists.js'
+import { createCharacter } from '../creation/create.js'
 
 const ROOMS = {
     '000': {
-        params: COMPLEXIONS.category,
-        description: listOptions(COMPLEXIONS),
-        commands: commands
-    },
-    '001': {
-        params: EYECOLORS.category,
-        description: listOptions(EYECOLORS),
-        commands: commands
-    },
-    '002': {
-        params: HAIRLENGTHS.category,
-        description: listOptions(HAIRLENGTHS),
-        commands: commands
-    },
-    '003': {
-        params: HAIRSTYLES.category,
-        description: listOptions(HAIRSTYLES),
-        commands: commands
-    },
-    '004': {
-        params: HAIRCOLORS.category,
-        description: listOptions(HAIRCOLORS),
-        commands: commands
+        title: '[The Womb: Moment of Creation]',
+        description: 'Blinding light permeates the atmosphere as you feel assured in your sense of purpose.',
+        script: createCharacter
     }
 }
 
