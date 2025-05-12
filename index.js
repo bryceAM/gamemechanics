b06f18a05eb2c4b3ae04f6993a77b3a7325db17c
bryceAM <bryce.malmberg@yahoo.com>
2025-05-11 22:32:19 -0500

    refactor the character in the code to be a broader client instance

--- index.js
+++ index.js
@@ -2,53 +2,19 @@ import { commands } from './utils/commands.js'
 import { ROOMS } from './world/rooms.js'
 
 // Initialize player character object
-class Character {
-    constructor(commands) {
-        this.commands = commands;
-    };
-    appearance = {
-        'complexion': null,
-        'eye color': null,
-        'hair length': null,
-        'hair style': null,
-        'hair color': null
-    };
-    race = {};
-    location = {
-        x: 0,
-        y: 0,
-        z: 0
-    };
-    look(room) {
-        if (room == null || room == undefined || Object.keys(room).length <= 0) {
-            commands.print("You don't see anything over there!");
-            commands.print();
-            return
-        }
-
-        const title = room.title;
-        const description = room.description;
-    
-        commands.print(title);
-        commands.print(description);
-        commands.print();
-    };
-    room = {};
-    move(room) {
-        if (room == null || room == undefined) {
-            commands.print("There's no way to get there!");
-            return
-        }
-        this.room = room;
-        this.look(this.room);
-    };
-    get description() {
-        const a = this.appearance;
-        return `You have a ${a['complexion']} complexion, ${a['eye color']} eyes, and ${a['hair length']} ${a['hair style']} ${a['hair color']} hair.`;
-    };
+class Client {
+    constructor() {
+        this.location = {
+            x: 0,
+            y: 0,
+            z: 0
+        };
+    }
+    character = {};
 }
 
-const character = new Character(commands);
+const client = new Client();
+const context = ['race', 'complexion', 'eye color', 'hair length', 'hair color', 'hair style'];
 
 (function initialize() {
     /**
@@ -57,8 +23,6 @@ const character = new Character(commands);
      */
 
     // Initialize
-    const xyz = `${character.location.x}${character.location.y}${character.location.z}`;
-    const room = ROOMS[xyz];
-    character.move(room);
-    character.room.script(character);
+    const xyz = `${client.location.x}${client.location.y}${client.location.z}`
+    ROOMS[xyz].script(client, context, commands);
 })()
